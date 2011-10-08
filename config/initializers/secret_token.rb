8244c1f03d068984bd6a182cf34c50831c6100ac
lucasmcastro <lucasmcastro@gmail.com>
2011-10-08 15:26:14 -0300

    import inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ItcursosEad::Application.config.secret_token = 'd9e7d15c095e29cb81c3c7f425b35ac66c0930f47d0dbf5742df8c61cba4e05accea400eb3539ba8c4bfdc6c68b021075840627e430191832615abcf2bebb431'
