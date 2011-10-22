ce16b928266e8ec727638a242e47b641e7d6b785
Brenno Costa <brenno.costa@me.com>
2011-10-22 16:12:41 -0300

    username|email login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 ItcursosEad::Application.routes.draw do
   devise_for :users
-
   resources :people
 
   # The priority is based upon order of creation:
@@ -53,7 +52,7 @@ ItcursosEad::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-  root :to => "home#index"
+  root :to => "people#index"
 
 
   # See how all your routes lay out with "rake routes"
